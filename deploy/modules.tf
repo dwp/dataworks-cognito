34e606f28a361aa1e4d399505cdfab8ed03a119a
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-05-18 15:17:17 +0100

    ci changes
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- deploy/modules.tf
+++ deploy/modules.tf
@@ -8,5 +8,5 @@ module "cognito" {
 
   root_dns_names = values(local.root_dns_name)
   domain         = local.cognito_domain
-  loadbalancer = "ci.wip.dataworks.dwp.gov.uk"
+  loadbalancer   = "ci.wip.dataworks.dwp.gov.uk"
 }
