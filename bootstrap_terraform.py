beb7b18c3b138951a6e984649d2afe6bbc2e44bc
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-05-18 17:06:07 +0100

    remove unused ssm client
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -20,11 +20,9 @@ def main():
     elif 'AWS_SECRETS_ROLE' in os.environ:
         secrets_session = assumed_role_session(os.environ['AWS_SECRETS_ROLE'])
     if 'AWS_REGION' in os.environ:
-        ssm = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
         secrets_manager = secrets_session.client(
             'secretsmanager', region_name=os.environ['AWS_REGION'])
     else:
-        ssm = boto3.client('ssm')
         secrets_manager = secrets_session.client('secretsmanager')
 
 
