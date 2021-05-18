0c4cb7ebd983163ddfbac1d1a79e49f7c837813e
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-05-18 15:33:53 +0100

    add the assume role method in bootstrap py
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -7,6 +7,8 @@ import os
 import sys
 import yaml
 import json
+import datetime
+from dateutil.tz import tzlocal
 
 
 def main():
@@ -57,6 +59,25 @@ def main():
     with open('deploy/terraform.tfvars', 'w+') as terraform_tfvars:
         terraform_tfvars.write(template.render(config_data))
     print("Terraform config successfully created")
+
+def assumed_role_session(role_arn: str, base_session: botocore.session.Session = None):
+    base_session = base_session or boto3.session.Session()._session
+    fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
+        client_creator=base_session.create_client,
+        source_credentials=base_session.get_credentials(),
+        role_arn=role_arn,
+        extra_args={
+            #    'RoleSessionName': None # set this if you want something non-default
+        }
+    )
+    creds = botocore.credentials.DeferredRefreshableCredentials(
+        method='assume-role',
+        refresh_using=fetcher.fetch_credentials,
+        time_fetcher=lambda: datetime.datetime.now(tzlocal())
+    )
+    botocore_session = botocore.session.Session()
+    botocore_session._credentials = creds
+    return boto3.Session(botocore_session=botocore_session)
 
 
 if __name__ == "__main__":
