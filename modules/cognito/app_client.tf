4b0aaa4589b578f1dda81e080f82f1f0dfbfe4de
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-05-18 16:45:15 +0100

    remove gen secret
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- modules/cognito/app_client.tf
+++ modules/cognito/app_client.tf
@@ -2,7 +2,6 @@ resource aws_cognito_user_pool_client app_client {
 
   name                                 = local.name
   user_pool_id                         = aws_cognito_user_pool.concourse.id
-  generate_secret                      = true
   allowed_oauth_flows_user_pool_client = true
   callback_urls                        = formatlist("https://%s/sky/issuer/callback", var.loadbalancer)
   supported_identity_providers         = ["COGNITO"]
