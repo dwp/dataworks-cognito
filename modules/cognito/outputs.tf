53297937bfcfcf8a89158dc31e1efea39d8d6725
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-09 17:13:33 +0100

    Fix outputs for tf13 (#7)
    * Fix outputs for tf13

--- modules/cognito/outputs.tf
+++ modules/cognito/outputs.tf
@@ -1,9 +1,15 @@
 output outputs {
   value = {
-    name                  = aws_cognito_user_pool_client.app_client.name
-    app_client            = aws_cognito_user_pool_client.app_client
-    user_pool_domain      = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
-    user_pool             = aws_cognito_user_pool.concourse
+    name = aws_cognito_user_pool_client.app_client.name
+    app_client = {
+      id            = aws_cognito_user_pool_client.app_client.id
+      client_secret = aws_cognito_user_pool_client.app_client.client_secret
+      user_pool_id  = aws_cognito_user_pool_client.app_client.user_pool_id
+    }
+    user_pool_domain = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
+    user_pool = {
+      id = aws_cognito_user_pool.concourse.id
+    }
     user_pool_main_domain = aws_cognito_user_pool_domain.main
     issuer                = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
   }
