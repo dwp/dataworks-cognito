ff658b5df5982d5fa63bacc1a598b04088097857
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-11-30 14:01:36 +0000

    add new pipeline-operators user group in cognito for SRE team (#10)
    * add new pipeline-operators user group in cognito for SRE team
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    * rename group
    
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    Co-authored-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- modules/cognito/user_groups.tf
+++ modules/cognito/user_groups.tf
@@ -13,3 +13,9 @@ resource "aws_cognito_user_group" "grafana_editor" {
   user_pool_id = aws_cognito_user_pool.concourse.id
   description  = "Grafana Editors"
 }
+
+resource aws_cognito_user_group sre {
+  name         = "sre"
+  user_pool_id = aws_cognito_user_pool.concourse.id
+  description  = "Concourse pipeline operators from the SRE team"
+}
